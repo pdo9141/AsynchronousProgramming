9f4d4b005b1d3d901e7f1cf36b316493acb7ac43
pdo9141 <pdo9141@gmail.com>
2016-05-28 12:24:27 -0700

    Add project files.

---
+++ MyLogin/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyLogin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginButton.IsEnabled = false;
+
+            var task = Task.Run(() => {
+                Thread.Sleep(2000);
+                return "Login Successful";
+            });
+
+            task.ContinueWith((t) => {
+                // Must use Dispatcher here since task thread cannot update UI thread components directly
+                Dispatcher.Invoke(() => {
+                    LoginButton.Content = t.Result;
+                    LoginButton.IsEnabled = true;
+                });
+            });
+        }
+
+        private void LoginAwaitButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginAwaitButton.IsEnabled = false;
+
+            var task = Task.Run(() => {                
+                Thread.Sleep(2000);
+                return "Login Successful";
+            });
+
+            // Configure continuation to continue on UI thread, the original context
+            task.ConfigureAwait(true)
+                .GetAwaiter()
+                .OnCompleted(() =>
+                {
+                    // No longer need to use Dispatcher since the continuation will happen on the original UI thread
+                    LoginAwaitButton.Content = task.Result;
+                    LoginAwaitButton.IsEnabled = true;
+                });
+        }
+
+        private void BadLoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            BadLoginButton.IsEnabled = false;
+
+            var task = Task.Run(() => {
+                throw new UnauthorizedAccessException();
+
+                Thread.Sleep(2000);
+                return "Login Successful";
+            });
+
+            task.ContinueWith((t) => {
+                if (t.IsFaulted)
+                {
+                    // Must use Dispatcher here since task thread cannot update UI thread components directly
+                    Dispatcher.Invoke(() =>
+                    {
+                        BadLoginButton.Content = "Login failed!";
+                        BadLoginButton.IsEnabled = true;
+                    });
+                }
+                else
+                {
+                    // Must use Dispatcher here since task thread cannot update UI thread components directly
+                    Dispatcher.Invoke(() =>
+                    {
+                        BadLoginButton.Content = t.Result;
+                        BadLoginButton.IsEnabled = true;
+                    });
+                }
+            });
+        }
+    }
+}
---
+++ ReliableWPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Windows;
+
+namespace ReliableWPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _count = 1;
+
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void RssButton_Click(object sender, RoutedEventArgs e)
+        {
+            RssButton.IsEnabled = false;
+
+            var client = new WebClient();
+            client.DownloadStringAsync(new System.Uri("http://www.filipekberg.se/rss/"));
+            client.DownloadStringCompleted += Client_DownloadStringCompleted;
+        }
+
+        private void Client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            RssText.Text = e.Result;
+            RssButton.IsEnabled = true;
+        }
+
+        private void CounterButton_Click(object sender, RoutedEventArgs e)
+        {
+            CounterText.Text = $"Counter: { _count++ }";
+        }
+    }
+}
